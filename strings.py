97ec889b1257ca201c7ff03fc96fd2701ad8456d
cyberbarbie <taeluralexis@gmail.com>
2020-05-09 16:31:14 -0400

    zero indexing in python and character selection

--- strings.py
+++ strings.py
@@ -1,2 +1,17 @@
+# to run script in terminal type python3 strings.py 
+
+# Strings are zero-indexed, each character in a string has a position and it starts from zero
+
 favorite_word = "fml"
+
+# select specific character in a string using [] and save it to a new variable
+first_element = favorite_word[0]
+
+# use print() to output text to the terminal
 print(favorite_word)
+
+print(first_element)
+
+my_name = "Cyberbarbie"
+
+first_initial = my_name[0]
