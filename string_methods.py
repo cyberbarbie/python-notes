25827e35536c8089e33137d168b19134ac64d320
cyberbarbie <taeluralexis@gmail.com>
2020-05-11 15:25:08 -0400

    added more info about string methods

--- string_methods.py
+++ string_methods.py
@@ -34,6 +34,78 @@ line_one_words = line_one.split()
 
 greatest_guitarist = "santana"
 # save the method to a new variable because strings are immutable
-new_stuff = greatest_guitarist.split('n')
+# every encounter of that specified character/substring start a new index
+new_stuff = greatest_guitarist.split('a')
 
-print(new_stuff)
+# output / ['s', 'nt', 'n', '']
+
+#print(new_stuff)
+
+twitch_name = "cyberbarbie"
+split_name = twitch_name.split('b')
+
+#print(split_name)
+
+favorite_food = "sushi"
+new_food = favorite_food.split('s')
+
+#print(new_food)
+
+authors = "Audre Lorde, William Carlos Williams, Gabriela Mistral, Jean Toomer, An Qi, Walt Whitman, Shel Silverstein, Carmen Boullosa, Kamala Suraiyya, Langston Hughes, Adrienne Rich, Nikki Giovanni"
+
+author_names = authors.split(',')
+
+#print(author_names)
+
+author_last_names = []
+
+for name in author_names:
+    author_last_names.append(name.split()[-1])
+
+#print(author_last_names)
+
+# Escape sequences- another way to escape/split a string
+# \n newline- create a line break
+
+# use the split method to turn the string into a list
+# we used the delimter \n to separate each line into its own element
+
+smooth_chorus = \
+"""And if you said, "This life ain't good enough."
+I would give my world to lift you up
+I could change my life to better suit your mood
+Because you're so smooth"""
+
+chorus_lines = smooth_chorus.split('\n')
+
+#print(chorus_lines)
+
+spring_storm_text = \
+"""The sky has given over 
+its bitterness. 
+Out of the dark change 
+all day long 
+rain falls and falls 
+as if it would never end. 
+Still the snow keeps 
+its hold on the ground. 
+But water, water 
+from a thousand runnels! 
+It collects swiftly, 
+dappled with black 
+cuts a way for itself 
+through green ice in the gutters. 
+Drop after drop it falls 
+from the withered grass-stems 
+of the overhanging embankment."""
+
+spring_storm_lines = spring_storm_text.split('\n')
+
+#print(spring_storm_lines)
+
+reapers_line_one_words = ["Black", "reapers", "with", "the", "sound", "of", "steel", "on", "stones"]
+
+reapers_line_one = ' '.join(reapers_line_one_words)
+
+print(reapers_line_one)
+
