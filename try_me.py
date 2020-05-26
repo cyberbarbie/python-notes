a21261814fef5ac4736806411aa4595a1b543aae
cyberbarbie <taeluralexis@gmail.com>
2020-05-26 15:11:53 -0400

    added coding challenge solutions

--- try_me.py
+++ try_me.py
@@ -1,14 +1,21 @@
+# Class- An object that provides a set of information
 class TwitchStreamer:
+    # class variables - variables defined instance of a class
     name = "Tae'lur"
     user_name = "Cyberbarbie"
     age = 23
     location = "Georgia"
 
+    # method()- a function defined inside of a class that can take multiple args
+    # self- the first argument; reference to whatever object it is called in
     def bio(self):
+        # prints a string with variables
         print(f"Hello my name is {self.name}, also known as {self.user_name} and I am {self.age}. I live in {self.location}.")
-
+# create an instance of an object
 cyberbarbie = TwitchStreamer()
 cyberbarbie.bio()
+
+#hasttr()- 
 
 # Access and change the properties on the instance (object)
 jyrone = TwitchStreamer()
